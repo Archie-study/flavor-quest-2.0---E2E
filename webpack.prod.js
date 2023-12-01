49db1e225f94a2e9c2fef14ec3a449b50a7d9f17
Archie-study <archiestudy12@gmail.com>
2023-12-01 23:39:01 +0700

    menambahkan optimization pada webpack production

--- webpack.prod.js
+++ webpack.prod.js
@@ -30,6 +30,31 @@ module.exports = merge(common, {
             maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         }),
     ],
+
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 20000,
+            maxSize: 70000,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            automaticNameDelimiter: '~',
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        },
+    },
+
     minimizer: [
         new ImageMinimizerPlugin({
             minimizer: {
